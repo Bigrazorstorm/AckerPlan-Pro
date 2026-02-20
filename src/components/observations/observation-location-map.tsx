c25548c334d05883cceb83aa080ec9cfa4c63af3
Marcel Kratzing <bigrazorstorm@gmail.com>
2026-02-20 20:12:08 +0000

    Try fixing this error: `Runtime Error: Map container is already initiali

--- src/components/observations/observation-location-map.tsx
+++ src/components/observations/observation-location-map.tsx
@@ -3,7 +3,7 @@
 import { MapContainer, TileLayer, Marker, Tooltip } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import * as L from 'leaflet';
-import { useEffect, useState, useMemo } from 'react';
+import { useMemo } from 'react';
 
 // Fix for default marker icon with webpack
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
@@ -23,17 +23,8 @@ interface ObservationLocationMapProps {
 
 export function ObservationLocationMap({ latitude, longitude }: ObservationLocationMapProps) {
     const position: L.LatLngExpression = [latitude, longitude];
-    const [isMounted, setIsMounted] = useState(false);
     
     const mapStyle = useMemo(() => ({ height: '100%', width: '100%', borderRadius: 'inherit', zIndex: 0 }), []);
-
-    useEffect(() => {
-      setIsMounted(true);
-    }, [])
-
-    if(!isMounted) {
-      return null;
-    }
 
     return (
         <MapContainer key={`${latitude}-${longitude}`} center={position} zoom={13} style={mapStyle} zoomControl={false} scrollWheelZoom={false} dragging={false}>
