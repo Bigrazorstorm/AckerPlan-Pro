d609c4d8e56efdda4f713120f67d55bbf98c5a45
Marcel Kratzing <bigrazorstorm@gmail.com>
2026-02-20 19:48:27 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '~l

--- src/components/fields/fields-map.tsx
+++ src/components/fields/fields-map.tsx
@@ -2,11 +2,23 @@
 
 import { MapContainer, TileLayer, Polygon, Tooltip, FeatureGroup, LayersControl, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import 'leaflet-defaulticon-compatibility';
 import { Field, Observation } from '@/services/types';
-import { LatLngExpression, LatLngTuple, latLng, latLngBounds } from 'leaflet';
+import L, { LatLngExpression, LatLngTuple, latLng, latLngBounds } from 'leaflet';
 import { useMemo } from 'react';
 import { useTranslations } from 'next-intl';
+
+// Fix for default marker icon with webpack
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon.src,
+  iconRetinaUrl: markerIcon2x.src,
+  shadowUrl: markerShadow.src,
+});
 
 interface FieldsMapProps {
     fields: Field[];
--- src/components/observations/observation-location-map.tsx
+++ src/components/observations/observation-location-map.tsx
@@ -2,8 +2,20 @@
 
 import { MapContainer, TileLayer, Marker, Tooltip } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import 'leaflet-defaulticon-compatibility';
 import * as L from 'leaflet';
+
+// Fix for default marker icon with webpack
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon.src,
+  iconRetinaUrl: markerIcon2x.src,
+  shadowUrl: markerShadow.src,
+});
 
 interface ObservationLocationMapProps {
     latitude: number;
